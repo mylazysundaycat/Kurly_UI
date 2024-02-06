2117cb9cf4c33e75d9b717cc96bdfce1304a2665
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-06 20:36:27 +0900

    Create. index.js 팝업창 삭제 기능 추가

---
+++ library/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("bb-x-icon").addEventListener("click", function() {
+    var banner = document.querySelector(".benefit-banner");
+    banner.parentNode.removeChild(banner);
+});
