6db3eebaf53755ddee87d1ba8e6de5e0b69a83f4
yunicode914 <yunicode914@gmail.com>
2024-02-06 11:25:55 +0900

    delete

--- JoinPage.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    const defaultWidth = 1440px;
-    const scale = window.screen.width / defaultWidth;
-    if (scale !== 1) {
-        document.body.style.transform = 'scale(' + scale + ')';
-    }
-}
