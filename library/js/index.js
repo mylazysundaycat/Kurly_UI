54f410ec5a5796f21d1bde12a3239dc892f4c8d6
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-08 02:27:15 +0900

    feat. 자바스크립트 장바구님 담기 중간저장

--- library/js/index.js
+++ library/js/index.js
@@ -2,3 +2,29 @@ document.getElementById("bb-x-icon").addEventListener("click", function() {
     var banner = document.querySelector(".benefit-banner");
     banner.parentNode.removeChild(banner);
 });
+
+
+document.getElementById("cancelButton").addEventListener("click", function() {
+    var popup = document.querySelector(".product-popup-p");
+    popup.style.display = "none";
+});
+
+document.getElementById("addToCartButton1").addEventListener("click", function() {
+    var popup = document.getElementById("product-popup-p");
+    popup.style.display = "block";
+});
+
+document.getElementById("addToCartButton2").addEventListener("click", function() {
+    var popup = document.getElementById("product-popup-p");
+    popup.style.display = "block";
+});
+
+document.getElementById("addToCartButton3").addEventListener("click", function() {
+    var popup = document.getElementById("product-popup-p");
+    popup.style.display = "block";
+});
+
+document.getElementById("addToCartButton4").addEventListener("click", function() {
+    var popup = document.getElementById("product-popup-p");
+    popup.style.display = "block";
+});
