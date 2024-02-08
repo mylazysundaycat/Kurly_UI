b0a3c4df212a049e5f73403d4e74e465492ecf5a
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-08 21:25:52 +0900

    Create 수량체크박스

--- library/js/index.js
+++ library/js/index.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     categoryBtn1.addEventListener("mouseover", function() {
         categoryBox1.style.display = "block";
-        categoryBox1.style.position = "fixed";
+        categoryBox1.style.position = "absolute";
     });
 
     // 마우스를 category-btn에서 뗐을 때
@@ -73,12 +73,114 @@ document.addEventListener("DOMContentLoaded", function() {
 
     categoryBtn2.addEventListener("mouseover", function() {
         categoryBox2.style.display = "block";
-        categoryBox2.style.position = "fixed";
+        categoryBox2.style.position = "absolute";
     });
 
     // 마우스를 category-btn에서 뗐을 때
     categoryBtn2.addEventListener("mouseout", function() {
         categoryBox2.style.display = "none";
         categoryBox2.style.position = "";
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartBtn = document.getElementById("addToCartBtn1");
+    const notification = document.getElementById("noti-container1");
+
+    // 장바구니 담기 버튼 클릭 시 알림 표시
+    addToCartBtn.addEventListener("click", function() {
+        // 알림 내용 설정
+        notification.style.display = "block";
+        // // 2초 후 알림 숨기기
+        // setTimeout(function() {
+        //     notification.style.display = "none";
+        // }, 2000);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const notiok = document.getElementById("noti-ok1");
+    const noticontainer1 = document.getElementById("noti-container1")
+
+    notiok.addEventListener("click", function() {
+        noticontainer1.style.display = "none";
+
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartBtn = document.getElementById("addToCartBtn2");
+    const notification = document.getElementById("noti-container2");
+
+    // 장바구니 담기 버튼 클릭 시 알림 표시
+    addToCartBtn.addEventListener("click", function() {
+        // 알림 내용 설정
+        notification.style.display = "block";
+        // // 2초 후 알림 숨기기
+        // setTimeout(function() {
+        //     notification.style.display = "none";
+        // }, 2000);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const notiok = document.getElementById("noti-ok2");
+    const noticontainer1 = document.getElementById("noti-container2")
+
+    notiok.addEventListener("click", function() {
+        noticontainer1.style.display = "none";
+
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartBtn = document.getElementById("addToCartBtn3");
+    const notification = document.getElementById("noti-container3");
+
+    // 장바구니 담기 버튼 클릭 시 알림 표시
+    addToCartBtn.addEventListener("click", function() {
+        // 알림 내용 설정
+        notification.style.display = "block";
+        // // 2초 후 알림 숨기기
+        // setTimeout(function() {
+        //     notification.style.display = "none";
+        // }, 2000);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const notiok = document.getElementById("noti-ok3");
+    const noticontainer1 = document.getElementById("noti-container3")
+
+    notiok.addEventListener("click", function() {
+        noticontainer1.style.display = "none";
+
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartBtn = document.getElementById("addToCartBtn4");
+    const notification = document.getElementById("noti-container4");
+
+    // 장바구니 담기 버튼 클릭 시 알림 표시
+    addToCartBtn.addEventListener("click", function() {
+        // 알림 내용 설정
+        notification.style.display = "block";
+        // // 2초 후 알림 숨기기
+        // setTimeout(function() {
+        //     notification.style.display = "none";
+        // }, 2000);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const notiok = document.getElementById("noti-ok4");
+    const noticontainer1 = document.getElementById("noti-container4")
+
+    notiok.addEventListener("click", function() {
+        noticontainer1.style.display = "none";
+
     });
 });
