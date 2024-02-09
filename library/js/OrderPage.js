69590424fea99946773f356dc181e677dd3db6b0
suuuuo <yunsm2385@naver.com>
2024-02-09 13:30:32 +0900

    주문화면 기능추가2

--- library/js/OrderPage.js
+++ library/js/OrderPage.js
@@ -40,11 +40,17 @@ function change_color(e){
 
 function use_all(e){ // 모두사용 버튼
     const max = 2000;
+    const cost = 219000;
     const point = parseInt(document.getElementById("point").value); //input 값 설정
     const btn = document.querySelectorAll(".Order_page-use_all_text");
+    const pay = document.getElementById("Order_page-pay_cost");
     btn.forEach(function (btn,i) {
         if(e.currentTarget == btn){
             document.getElementById("point").value = comma(max);
+            document.getElementById("point_sum").value = comma(max);
+            document.getElementById("point_sum_all").value = comma(max);
+            document.getElementById("final_cost_id").value = comma(cost - max);
+            pay.innerText = comma(cost - max).toString()+"원 결제하기";
         }else{
         }
     });
@@ -52,14 +58,23 @@ function use_all(e){ // 모두사용 버튼
 
 function check_point(){ // 쉼표 추가, max 넘기면 max 자동 입력
     const max = 2000;
+    const cost = 219000;
     const point = parseInt(document.getElementById("point").value); //input 값 설정
+    const pay = document.getElementById("Order_page-pay_cost");
     if(point > max){
         document.getElementById("point").value = comma(max);
+        document.getElementById("point_sum").value = comma(max);
+        document.getElementById("point_sum_all").value = comma(max);
+        document.getElementById("final_cost_id").value = comma(cost-max);
+        pay.innerText = comma(cost - max).toString()+"원 결제하기";
     }else{
         document.getElementById("point").value = comma(point);
+        document.getElementById("point_sum").value = comma(point);
+        document.getElementById("point_sum_all").value = comma(point);
+        document.getElementById("final_cost_id").value = comma(cost-point);
+        pay.innerText = comma(cost - point).toString()+"원 결제하기";
     }
 }
-
 
 //, 찍는 함수 - 예제코드 참고
 function comma(num) {
