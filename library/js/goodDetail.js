1d6294b9e49b41621667342960f2d3e8a4d75df4
Juyoung <syseoz@gmail.com>
2024-02-09 17:44:05 +0900

    goodDetail 리뷰 모달창 수정

--- library/js/goodDetail.js
+++ library/js/goodDetail.js
@@ -8,31 +8,33 @@ window.addEventListener("DOMContentLoaded", function () {
       parseInt(data_price) * parseInt($(".inp").val())
     ).toLocaleString("ko-KR");
 
-    //수량 증가 감소
+    //수량 감소 버튼 클릭
     $(".down").click(function () {
       if (parseInt($(".inp").val()) <= 1) {
         $(".inp").val(1);
         alert("수량");
         return;
       }
-
       number--;
       $(".inp").val(number);
-      console.log(parseInt(data_price) * parseInt($(".inp").val()));
 
-      total_price.innerHTML = (
-        parseInt(data_price) * parseInt($(".inp").val())
-      ).toLocaleString("ko-KR");
+      changeTotalPrice();
     });
 
+    //수량 증가 버튼 클릭
     $(".up").click(function () {
       number++;
       $(".inp").val(number);
 
+      changeTotalPrice();
+    });
+
+    //수량 입력시 총 가격 변경
+    function changeTotalPrice() {
       total_price.innerHTML = (
         parseInt(data_price) * parseInt($(".inp").val())
       ).toLocaleString("ko-KR");
-    });
+    }
 
     // 상품 상세정보 탭 이동
     $(".goods-view-infomation-tab-anchor").click(function () {
@@ -54,12 +56,12 @@ window.addEventListener("DOMContentLoaded", function () {
     const mainBackground = document.getElementById("container");
     const modalCloseBtn = document.getElementById("modal_btnClose");
 
-    //  open modal
+    // 모달 열기
     function openModal() {
       modalContainer.style.display = "flex";
     }
 
-    //  close modal
+    //  모달 닫기 버튼
     function closeModal() {
       modalContainer.style.display = "none";
       background.style.display = "none";
@@ -71,10 +73,88 @@ window.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".gr_thumb").forEach((item) => {
       console.log("click");
       item.addEventListener("click", openModal);
-      //   // document.querySelector(".gr_thumb.active").classList.remove("active");
-      //   // item.classList.add("active");
-      //   // document.querySelector(".goods-view-img").src = item.src;
-      // });
     });
+
+    // 모달 데이터 바인딩
+    const mr = document.getElementById("modal_review_img");
+    const mr_product_name = document.getElementById("gr_a_product_name");
+    const mr_review = document.getElementById("gr_a_content_detail");
+
+    let n = 0;
+    document
+      .getElementById("modal_next_btn")
+      .addEventListener("click", function () {
+        if (n >= modalMockData.length) {
+          n = 0;
+        }
+        changeModalContent();
+        n = n + 1;
+      });
+
+    document
+      .getElementById("modal_prev_btn")
+      .addEventListener("click", function () {
+        if (n <= 0) {
+          n = modalMockData.length;
+        }
+        changeModalContent();
+        n = n - 1;
+      });
+
+    function changeModalContent() {
+      mr.setAttribute("src", modalMockData[n].src);
+      mr_product_name.innerHTML = modalMockData[n].good_title;
+      mr_review.innerHTML = modalMockData[n].good_review;
+    }
   });
+  const modalMockData = [
+    {
+      id: 1,
+      src: "/library/img/gd_product_3.jpg",
+      good_title: "테스트 상품1",
+      good_review: "테스트 리뷰1",
+    },
+    {
+      id: 2,
+      src: "/library/img/gd_product_2.jpg",
+      good_title: "테스트 상품2",
+      good_review: "테스트 리뷰2",
+    },
+    {
+      id: 3,
+      src: "/library/img/gd_product_3.jpg",
+      good_title: "테스트 상품3",
+      good_review: "테스트 리뷰3",
+    },
+    {
+      id: 4,
+      src: "/library/img/gd_product_2.jpg",
+      good_title: "테스트 상품4",
+      good_review: "테스트 리뷰4",
+    },
+    {
+      id: 5,
+      src: "/library/img/gd_product_3.jpg",
+      good_title: "테스트 상품5",
+      good_review: "테스트 리뷰5",
+    },
+    {
+      id: 6,
+      src: "/library/img/gd_product_2.jpg",
+      good_title: "테스트 상품6",
+      good_review: "테스트 리뷰6",
+    },
+    {
+      id: 7,
+      src: "/library/img/gd_product_2.jpg",
+      good_title: "테스트 상품7",
+      good_review: "테스트 리뷰7",
+    },
+    {
+      id: 8,
+      src: "/library/img/gd_product_3.jpg",
+      good_title: "테스트 상품8",
+      good_review: "테스트 리뷰8",
+    },
+  ];
 });
